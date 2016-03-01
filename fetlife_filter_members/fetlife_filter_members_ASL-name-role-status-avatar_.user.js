2e3d62075822d4a039243ec46a8cf707db31e83f
bewam <bewam@free.fr>
2016-03-01 12:39:54 +0100

    update metas

--- fetlife_filter_members/fetlife_filter_members_ASL-name-role-status-avatar_.user.js
+++ fetlife_filter_members/fetlife_filter_members_ASL-name-role-status-avatar_.user.js
@@ -3,7 +3,8 @@
 // @namespace    io.github.bewam
 // @description    greasemonkey script to filter fetlife members when it's possible. Search by name, gender, role, age, location or status.
 // @include      http*://fetlife.com/*
-// @version      1.9.2.20160229
+// @updateURL  https://github.com/bewam/userscripts/raw/master/fetlife_filter_members/fetlife_filter_members_ASL-name-role-status-avatar_.user.js
+// @version      1.9.2.20160301
 // @grant      GM_addStyle
 // @run-at      document-end
 // @require      http://code.jquery.com/jquery-2.1.1.min.js
